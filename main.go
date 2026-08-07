5d5b23e517ef384cb2840f13e8eb24032db20565
CtrlZExpert <vernett.cartwright@gmail.com>
2026-08-07 19:31:54 -0400

    Add internet conectivity check

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func checkArch() bool {
@@ -17,12 +19,33 @@ func checkArch() bool {
 	
 }
 
+
+func checkInternet() bool {
+	client := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	resp, err := client.Get("https://google.com")
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return true
+}
+
 func main() {
 	isArch := checkArch()
 	if isArch {
 		fmt.Println("Arch Linux: OK")
 	} else {
 		fmt.Println("Arch Linux: FAILED")
+	}
+
+	isInternet := checkInternet()
+	if isInternet {
+		fmt.Println("Internet: OK")
+	} else {
+		fmt.Println("Internet: Failed")
 	}
 
 }
